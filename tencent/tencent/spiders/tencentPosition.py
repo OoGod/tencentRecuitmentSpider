7e0027964718c700bbd52c9cf365bfc7e7f193fb
OoGod <1617605689@qq.com>
2019-03-14 20:49:19 +0800

    create tencentPosition spider

--- tencent/tencent/spiders/tencentPosition.py
+++ tencent/tencent/spiders/tencentPosition.py
@@ -8,4 +8,25 @@ class TencentpositionSpider(scrapy.Spider):
     start_urls = ['http://tencent.com/']
 
     def parse(self, response):
+        for each in response.xpath("//tr[@class='even'] | //tr[@class='odd']"):
+            # 初始化模型对象
+            item = TencentItem()
+
+            item['positionname'] = each.xpath("./td[1]/a/text()").extract()[0]
+            # 详情连接
+            item['positionlink'] = each.xpath("./td[1]/a/@href").extract()[0]
+            # 职位类别
+            item['positionType'] = each.xpath("./td[2]/text()").extract()[0]
+            # 招聘人数
+            item['peopleNum'] =  each.xpath("./td[3]/text()").extract()[0]
+            # 工作地点
+            item['workLocation'] = each.xpath("./td[4]/text()").extract()[0]
+            # 发布时间
+            item['publishTime'] = each.xpath("./td[5]/text()").extract()[0]
+        
+            yield item
+       if self.offset <= 3087:
+           self.offset += 10 
+       
+       yield scrapy.Request(self.url + str(self.offset),callback=self.parse) 
         pass
